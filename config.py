6e752096e3d3929cff2687f7f3eefcd25da24349
rernst <r.f.ernst-3@umcutrecht.nl>
2018-08-21 14:35:37 +0200

    New config items.

--- config.py
+++ config.py
@@ -1,5 +1,10 @@
 """Clarity Portal configuration."""
 
+# Website
 SECRET_KEY = 'change_this'
-
 REMOVED_SAMPLES_FILE = 'path/to/removed_samples.txt'
+
+# Genologics
+BASEURI = 'change_this'
+USERNAME = 'change_this'
+PASSWORD = 'change_this'
