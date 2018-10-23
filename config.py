54062b7e23bd924a76cea3f13b190bec302697a4
rernst <r.f.ernst-3@umcutrecht.nl>
2018-10-23 14:09:44 +0200

    Update config

--- config.py
+++ config.py
@@ -6,10 +6,16 @@ REMOVED_SAMPLES_FILE = 'path/to/removed_samples.txt'
 LIMS_INDICATIONS = {
     'PD-PMC001': {
         'project_name_prefix': 'PMC_DXweek',
-        'sample_name_prefixes': ['PMCBM', 'PMGBM'],
+        'sample_name_prefixes': ['PM'],
+        'workflow_id': '701',  # Dx Sequence facility v1.1
+    },
+    'RNA_seq': {
+        'project_name_prefix': 'DX_RNASeq_week',
+        'sample_name_prefixes': ['U'],
         'workflow_id': '701',  # Dx Sequence facility v1.1
     }
 }
+
 
 # Genologics
 BASEURI = 'change_this'
