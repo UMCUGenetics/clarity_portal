dcf5b802118a621496cb05c9d7b3b0c2b19b6a57
Robert Ernst <r.f.ernst-3@umcutrecht.nl>
2024-05-23 14:36:26 +0200

    Decode input file

--- portal/forms.py
+++ portal/forms.py
@@ -265,6 +265,7 @@ class SubmitDXSampleForm(FlaskForm):
         }
 
         for line in self.helix_worklist.data:
+            line = line.decode('utf-8')
             if not header:
                 header = line.rstrip().split(',')
                 for udf in udf_column:
